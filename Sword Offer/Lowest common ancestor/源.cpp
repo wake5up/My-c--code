31c7b29c0cb6926733524ddb10f1f687396ff18b
郝团团 <2740276135@qq.com>
2020-03-16 17:56:47 +0800

    最低公共祖先

---
+++ Sword Offer/Lowest common ancestor/源.cpp
@@ -0,0 +1,27 @@
+//树中两节点的最低公共祖先
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(!root||p==root||q==root)
+            return root;
+        TreeNode* l=lowestCommonAncestor(root->left,p,q);
+        TreeNode* r=lowestCommonAncestor(root->right,p,q);
+        if(!l&&!r)
+            return nullptr;
+        else if(!l&&r)
+            return r;
+        else if(l&&!r)
+            return l;
+        else
+            return root;
+    }
+};
