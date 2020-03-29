fa8d59a7eb5f5c85d535982f1474a37422ba2120
郝团团 <2740276135@qq.com>
2020-03-29 22:36:58 +0800

    在线OJ项目

---
+++ OJ-project/oj_server.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <string>
+
+#include "httplib.h"
+#include "oj_model.hpp"
+#include "oj_view.hpp"
+#include "oj_log.hpp"
+
+int main()
+{
+    using namespace httplib;
+    Server svr;
+
+    return 0;
+}
---
+++ OJ-project/oj_view.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include <ctemplate/template.h>
+#include <string>
+#include <vector>
+#include "oj_model.hpp"
+
+class OjView
+{
+    public:
+};
